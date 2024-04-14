7185fca8592e7818d569ed8de24c5099b373e8bf
NashaFrith <44258016+NashaFrith@users.noreply.github.com>
2024-04-13 21:59:57 -0500

    Update project to reflect changes made to tables

---
+++ script.py
@@ -0,0 +1,149 @@
+from faker import Faker
+from faker.providers import DynamicProvider
+import random
+
+
+fake = Faker()
+sqls = []
+cids = []
+lecturers = []
+lecmax = []
+fids = []
+mtids = []
+
+course_provider = DynamicProvider(
+    provider_name = "course",
+    elements = ["Intro to Computing", "Object-oriented Programming","Math for Computing","Computing and Society", "Discrete Mathematics for Computer Science","Analysis of Algorithms","Digital Logic Design","Software Engineering","Object Technology","Net-Centric Computing","Computer Organisation"," Operating Systems","Introduction to Artificial Intelligence","Database Management Systems","Language Processors","Theory of Computation","Real-Time Embedded Systems","Group Project","Internship in Computing","Project Management"]
+)
+
+fake.add_provider(course_provider)
+
+def real_job(fake):
+    job = fake.job()
+    job = job.replace("'","")
+    return job
+
+#INSERT ADMIN INTO ACCOUNT
+for i in range(1):
+    id = 9000 + i
+    type = "Admin"
+    pa = type + id
+    sql = "INSERT INTO Account (UserID, uType, Pass) VALUES ('{}','{}','{}')".format(id, type, pa)
+    sqls.append(sql)
+
+
+#INSERT INTO STUDENT AND ACCOUNT
+for i in range(10000):
+    id = i
+    fname = fake.first_name()
+    lname= fake.last_name()
+    type = "Student"
+    pa = fname + fake.number()
+    sql = "INSERT INTO Student (StudentID, FirstName, LastName) VALUES ('{}','{}','{}')".format(id,fname,lname)
+    sqls.append(sql)
+    sql = "INSERT INTO Account (UserID, uType, Pass) VALUES ('{}','{}','{}')".format(id, type, pa)
+    sqls.append(sql)
+
+
+#INSERT INTO LECTURER AND ACCOUNT
+for _ in range(3):
+    lid = random.randint(1011,1050)
+    name = fake.last_name()
+    lecturers.append((lid,name)) 
+    pa = name + fake.number()
+    type = "Lecturer"
+
+    sql = "INSERT INTO LECTURER (LecID, LecName) VALUES ('{}', '{}')".format(lid, name)
+    sqls.append(sql)
+    sql = "INSERT INTO Account (UserID, uType, Pass) VALUES ('{}','{}','{}')".format(lid, type, pa)
+    sqls.append(sql)
+
+
+#INSERT INTO COURSE   
+for i in range(20):
+    cid = 3001 + i
+    c_code = "COMP" + str(cid)
+    cname = fake.unique.course()
+    cids.append(cid)
+
+for cid in cids:
+    lid, name = lecturers.pop(0)
+    lecturers.append((lid,name))
+
+    sql = "INSERT INTO Course (CourseID, CourseName, CourseCode, LecID) VALUES ('{}','{}','{}','{})".format(cid,cname,c_code,lid)
+    sqls.append(sql)
+
+#INSERT INTO SECTION
+for cid in cids:
+    for i in range(0,2):
+        secid = i +1
+        sql = "INSERT INTO Section(CourseID, SectionID) VALUES ('{}','{}')".format(cid,secid)
+        sqls.append(sql)
+
+'''
+for i in range(30):
+    cid = 4001 + i
+    c_code = "INFO" + str(cid)
+    cname = "IT" + " " + real_job(fake)
+    sql = "INSERT INTO Course (CourseID, CourseName, CourseCode) VALUES ('{}','{}','{}')".format(cid,cname,c_code)
+    sqls.append(sql)
+    cids.append(cid)
+'''
+
+#INSERT INTO ASSIGN
+for s in sqls:
+    if s.startswith("INSERT INTO Student"):
+        id = s.split("'")[1]
+
+        as_courses = set()
+        i = random.randint(3,6)
+        for _ in range(i):
+            while True:
+                cid = random.choice(cids)
+                if cid not in as_courses:
+                    as_courses.add(cid)
+                    break
+            grade = random.randint(0,100)
+            sql = "INSERT INTO Assign (CourseID, StudentID, AvGrade) VALUES ('{}','{}','{}')".format(cid,id,grade)
+            sqls.append(sql)
+
+#INSERT INTO FORUM
+for i in range(5):
+    fid = i
+    fids.append(fid)
+    cid = random.choice(cids)
+    fname = "What do you think about" + fake.unique.color()
+    sql = "INSERT INTO Forum (ForumID, CourseID, ForumName) VALUES ('{}','{}','{}')".format(fid,cid,fname)
+    sqls.append(sql)
+
+#INSERT INTO THREAD
+for i in range(2):
+    tid = 100 + random.randint()
+    fid = random.choice(fids)
+    title = fake.sentence(10)
+    body = fake.sentence(50)
+    mtids.append(tid)
+
+    sql = "INSERT INTO Thread (ThreadID, ForumID, Title, Body) VALUES ('{}','{}','{}','{}')".format(tid,fid,title,body)
+    sqls.append(sql)
+
+#INSERT INTO REPLY
+for i in range(2):
+    mtid = random.choice(mtids)
+    rid = 200 + random.randint()
+    body = fake.sentence(20)
+    
+    for s in sqls:
+        if s.startswith("INSERT INTO Account"):
+            id = s.split("'")[1]
+            sql = "INSERT INTO Reply (MainThreadID, ReplyID, UserID,ReplyBody) VALUES ('{}','{}','{}','{}')".format(mtid,rid,id,body)
+    sqls.append(sql)
+
+#INSERT INTO EVENT
+
+
+
+
+with open(r'/home/nacho/repos/COMP3161FinalProject/insert.sql','w') as f:
+    for i, sql in enumerate(sqls):
+        f.write(sql + '\n')
