327e921bd0fc162d7a8347a9f1ca87f0e6165399
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2024-04-25 02:39:32 -0500

    Small changes

--- insert.sql
+++ insert.sql
@@ -1,24 +1,26 @@
 --Create Database
+DROP DATABASE IF EXISTS uwi;
 CREATE DATABASE uwi;
-GRANT ALL PRIVILEGES ON uwi TO nacho@localhost;
+USE uwi;
+GRANT ALL PRIVILEGES ON uwi TO comp3161@localhost;
 
 
 --Create Tables
 --input will be updated during script creation
-CREATE Table Account (UserID int, uType varchar(8), Pass varchar(255), FirstName varchar(255), LastName varchar(255)) 
+CREATE Table Account (UserID int, uType varchar(8), Pass varchar(255), FirstName varchar(255), LastName varchar(255));
 
 
 CREATE Table Course (CourseID int, CourseName varchar(80), CourseCode varchar(255));
 CREATE Table Section (CourseID int, SectionID int);
-CREATE Table Item(SectionID int, ItemID int, title int, itype varchar(20));
+CREATE Table Item(SectionID int, ItemID int, title varchar(80), itype varchar(20));
 CREATE Table Teaches(CourseID int, UserID int);
 
 CREATE Table Assignment (AssID int, UserID int, CourseID int, Grade int, date_submit date);
-CREATE Table Enroll (CourseID int, StudentID int, AvGrade int); --course averages
+CREATE Table Enroll (CourseID int, StudentID int); --course averages
 
 CREATE Table Event (EventID int, CourseID int, EventName varchar(255),Duedate date);
 
 
 CREATE Table Forum (ForumID int, CourseID int, ForumName varchar(255));
-CREATE Table Thread (ThreadID int, ForumID int, Title varchar(80), Body varchar(255), created_by varchar(30)); --if parentID=0, it's the main thread
-CREATE Table Replies (MainThreadID int, ReplyID int, ReplyBody varchar(255), created_by varchar(30));
+CREATE Table Thread (ThreadID int, ForumID int, Title varchar(80), Body varchar(2048), created_by varchar(30)); --if parentID=0, it's the main thread
+CREATE Table Replies (MainThreadID int, ReplyID int, ReplyBody varchar(2048), created_by varchar(30));
--- script.py
+++ script.py
@@ -7,24 +7,27 @@ fake = Faker()
 sqls = []
 sqlsStu = []
 sqlsAcc = []
+ids = []
 cids = []
-lecturers = []
+lids = []
 lecmax = []
 fids = []
-mtids = []
+tids = []
 secids = []
+as_courses = []
 
 course_provider = DynamicProvider(
     provider_name = "course",
     elements = ["Intro to Computing", "Object-oriented Programming","Math for Computing","Computing and Society", "Discrete Mathematics for Computer Science","Analysis of Algorithms","Digital Logic Design","Software Engineering","Object Technology","Net-Centric Computing","Computer Organisation"," Operating Systems","Introduction to Artificial Intelligence","Database Management Systems","Language Processors","Theory of Computation","Real-Time Embedded Systems","Group Project","Internship in Computing","Project Management"]
 )
 
-course_provider = DynamicProvider(
+item_provider = DynamicProvider(
     provider_name = "item_type",
     elements = ["link", "file", "slide"]
 )
 
 fake.add_provider(course_provider)
+fake.add_provider(item_provider)
 
 def real_job(fake):
     job = fake.job()
@@ -38,16 +41,18 @@ for i in range(1):
     lname= fake.last_name()
     type = "Admin"
     pa = type + str(id)
-    sql = "INSERT INTO Account (UserID, uType, Pass, FirstName, LastName) VALUES ('{}','{}','{}','{}','{}')".format(id, type, pa,fname,lname)
+    sql = "INSERT INTO Account (UserID, uType, Pass, FirstName, LastName) VALUES ('{}','{}','{}','{}','{}');".format(id, type, pa,fname,lname)
     sqls.append(sql)
 
+id = 0
 for i in range(10000):
-    id = i
+    id += 1
+    ids.append(id)
     fname = fake.first_name()
     lname= fake.last_name()
     type = "Student"
     pa = fname + str(random.randint(0,200))
-    sql = "INSERT INTO Account (UserID, uType, Pass, FirstName, LastName) VALUES ('{}','{}','{}','{}','{})".format(id, type, pa,fname,lname)
+    sql = "INSERT INTO Account (UserID, uType, Pass, FirstName, LastName) VALUES ('{}','{}','{}','{}','{}');".format(id, type, pa,fname,lname)
     sqls.append(sql)
 
 
@@ -55,11 +60,11 @@ for _ in range(3):
     lid = random.randint(1011,1050)
     fname = fake.first_name()
     lname= fake.last_name()
-    lecturers.append(lid) 
+    lids.append(lid) 
     pa = fname + str(random.randint(0,20))
     type = "Lecturer"
 
-    sql = "INSERT INTO Account (UserID, uType, Pass, FirstName, LastName) VALUES ('{}','{}','{}','{}','{}')".format(lid, type, pa,fname,lname)
+    sql = "INSERT INTO Account (UserID, uType, Pass, FirstName, LastName) VALUES ('{}','{}','{}','{}','{}');".format(lid, type, pa,fname,lname)
     sqls.append(sql)
 
 
@@ -71,14 +76,15 @@ for i in range(20):
     cids.append(cid)
 
 for cid in cids:
-    sql = "INSERT INTO Course (CourseID, CourseName, CourseCode) VALUES ('{}','{}','{}')".format(cid,cname,c_code)
+    sql = "INSERT INTO Course (CourseID, CourseName, CourseCode) VALUES ('{}','{}','{}');".format(cid,cname,c_code)
     sqls.append(sql)
 
 #INSERT INTO SECTION
 for cid in cids:
     for i in range(0,2):
         secid = i +1
-        sql = "INSERT INTO Section(CourseID, SectionID) VALUES ('{}','{}')".format(cid,secid)
+        secids.append(secid)
+        sql = "INSERT INTO Section(CourseID, SectionID) VALUES ('{}','{}');".format(cid,secid)
         sqls.append(sql)
 
 #INSERT INTO ITEM
@@ -86,22 +92,22 @@ for secid in secids:
     for i in range(0,4):
         itemid = i + 20
         title = "Learning File" + " " + str(i)
-        type = fake.unique.item_type()
-        sql = "INSERT INTO Section(SectionID,ItemID,title,itype) VALUES ('{}','{}','{}','{}')".format(secid,itemid,title,type)
-
+        type = fake.item_type()
+        sql = "INSERT INTO Item(SectionID,ItemID,title,itype) VALUES ('{}','{}','{}','{}');".format(secid,itemid,title,type)
+        sqls.append(sql)
 
 #INSERT INTO TEACHES
 for cid in cids:
-    lid = lecturers.pop(0)
-    lecturers.append(lid)
-    sql = "INSERT INTO Teaches (CourseID, UserID) VALUES ('{}','{}')".format(cid,lid)
-
+    lid = lids.pop(0)
+    lids.append(lid)
+    sql = "INSERT INTO Teaches (CourseID, UserID) VALUES ('{}','{}');".format(cid,lid)
+    sqls.append(sql)
 '''
 for i in range(30):
     cid = 4001 + i
     c_code = "INFO" + str(cid)
     cname = "IT" + " " + real_job(fake)
-    sql = "INSERT INTO Course (CourseID, CourseName, CourseCode) VALUES ('{}','{}','{}')".format(cid,cname,c_code)
+    sql = "INSERT INTO Course (CourseID, CourseName, CourseCode) VALUES ('{}','{}','{}');".format(cid,cname,c_code)
     sqls.append(sql)
     cids.append(cid)
 '''
@@ -110,20 +116,17 @@ for i in range(30):
 for s in sqls:
     if s.startswith("INSERT INTO Account"):
         id = s.split("'")[1]
-        type = s.split("'")[2]
-
-        if type == "Student":
-            as_courses = set()
-            i = random.randint(3,6)
-
+        type = s.split("'")[3]
+    
+        if type == "Student":          
+            i = random.randint(3,6) 
             for _ in range(i):
-                while True:
-                    cid = random.choice(cids)
-                    if cid not in as_courses:
-                        as_courses.add(cid)
-                        break
+                cid = random.choice(cids)
+                if cid not in as_courses:
+                    as_courses.append(cid)
+                    break
                 grade = random.randint(0,100)
-                sql = "INSERT INTO Enroll (CourseID, StudentID, AvGrade) VALUES ('{}','{}','{}')".format(cid,id,grade)
+                sql = "INSERT INTO Enroll (CourseID, StudentID) VALUES ('{}','{}');".format(cid,id)
                 sqls.append(sql)
 
 #INSERT INTO ASSIGNMENT
@@ -131,13 +134,14 @@ i = 1
 for s in sqls:
     if s.startswith("INSERT INTO Enroll"):
         cid = s.split("'")[1]
-        id = s.split("'")[2]
+        id = s.split("'")[3]
+        #print(id)
         aid = 1111 + i
         i+=1
         grade = random.randint(0,100)
         sdate = fake.date()
 
-        sql = "INSERT INTO Assignment (AssID, UserID, StudentID,Grade,date_submit) VALUES ('{}','{}','{}','{}','{}')".format(aid,id,cid,grade,sdate)
+        sql = "INSERT INTO Assignment (AssID, UserID, CourseID,Grade,date_submit) VALUES ('{}','{}','{}','{}','{}');".format(aid,id,cid,grade,sdate)
         sqls.append(sql)
 
 
@@ -148,7 +152,7 @@ for cid in cids:
     ddate = fake.date()
     i + 1
     ename = "Assignment" + " " + str(i)
-    sql = "INSERT INTO Event (EventID, CourseID, EventName, DueDate) VALUES ('{}','{}','{}','{}')".format(eid,cid,ename,ddate)
+    sql = "INSERT INTO Event (EventID, CourseID, EventName, Duedate) VALUES ('{}','{}','{}','{}');".format(eid,cid,ename,ddate)
     sqls.append(sql)
 
 #INSERT INTO FORUM
@@ -157,7 +161,7 @@ for i in range(5):
     fids.append(fid)
     cid = random.choice(cids)
     fname = "What do you think about" + fake.unique.color()
-    sql = "INSERT INTO Forum (ForumID, CourseID, ForumName) VALUES ('{}','{}','{}')".format(fid,cid,fname)
+    sql = "INSERT INTO Forum (ForumID, CourseID, ForumName) VALUES ('{}','{}','{}');".format(fid,cid,fname)
     sqls.append(sql)
 
 #INSERT INTO THREAD
@@ -166,25 +170,23 @@ for i in range(2):
     fid = random.choice(fids)
     title = fake.sentence(10)
     body = fake.sentence(50)
-    mtids.append(tid)
+    tids.append(tid)
+    id = random.choice(ids)
 
-    sql = "INSERT INTO Thread (ThreadID, ForumID, Title, Body,created_by) VALUES ('{}','{}','{}','{}')".format(tid,fid,title,body)
+    sql = "INSERT INTO Thread (ThreadID, ForumID, Title, Body,created_by) VALUES ('{}','{}','{}','{}','{}');".format(tid,fid,title,body,id)
     sqls.append(sql)
 
-#INSERT INTO REPLY
+#INSERT INTO REPLIES
 for i in range(2):
-    mtid = random.choice(mtids)
+    tid = random.choice(tids)
     rid = 200 + random.randint(0,200)
     body = fake.sentence(20)
+    id = random.choice(ids)
     
-    for s in sqls:
-        if s.startswith("INSERT INTO Account"):
-            if type == "Student":
-                name = (s.split("'")[4]) + (s.split("'")[5])
-                sql = "INSERT INTO Reply (MainThreadID, ReplyID,ReplyBody,created_by) VALUES ('{}','{}','{}','{}')".format(mtid,rid,body,name)
-                sqls.append(sql)
+    sql = "INSERT INTO Replies (MainThreadID, ReplyID,ReplyBody,created_by) VALUES ('{}','{}','{}','{}');".format(tid,rid,body,id)
+    sqls.append(sql)
 
 
-with open(r'/home/nacho/repos/COMP3161FinalProject/insert.sql','w') as f:
+with open(r'insert.sql','a') as f:
     for i, sql in enumerate(sqls):
         f.write(sql + '\n')
