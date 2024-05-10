c1336c7299dc989a219d87c668812163b4578ca9
Brittanie Morgan <92182867+brittaniemorgan@users.noreply.github.com>
2024-05-09 21:21:36 -0500

    added validation sql queries

---
+++ validation.sql
@@ -0,0 +1,39 @@
+-- You must have at least 100,000 students.
+SELECT COUNT(*) FROM Account WHERE uType = 'Student';
+
+-- You must have at least 200 courses.
+SELECT COUNT(*) FROM Course; 
+
+-- No student can do more than 6 courses
+SELECT FirstName, LastName, COUNT(CourseID) FROM Account a
+JOIN Enroll e ON a.UserID = e.StudentID
+WHERE uType = 'Student'
+GROUP BY FirstName, LastName
+HAVING COUNT(CourseID) > 6;
+
+-- A student must be enrolled in at least 3 courses.
+SELECT FirstName, LastName, COUNT(CourseID) FROM Account a
+JOIN Enroll e ON a.UserID = e.StudentID
+WHERE uType = 'Student'
+GROUP BY FirstName, LastName
+HAVING COUNT(CourseID) < 3;
+
+-- Each course must have at least 10 members.
+SELECT CourseCode, CourseName, COUNT(StudentID) FROM Course c
+JOIN Enroll e ON c.CourseID = e.CourseID
+GROUP BY CourseCode, CourseName
+HAVING COUNT(StudentID) < 10;
+
+-- No lecturer can teach more than 5 courses.
+SELECT FirstName, LastName, COUNT(CourseID) FROM Account a
+JOIN Teaches t ON a.UserID = t.UserID
+WHERE uType = 'Teacher'
+GROUP BY FirstName, LastName
+HAVING COUNT(CourseID) > 5;
+
+-- A lecturer must teach at least 1 course.
+SELECT FirstName, LastName, COUNT(CourseID) FROM Account a
+JOIN Teaches t ON a.UserID = t.UserID
+WHERE uType = 'Teacher'
+GROUP BY FirstName, LastName
+HAVING COUNT(CourseID) < 1;
